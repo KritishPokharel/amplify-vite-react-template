846db225843a6c0c159879b482934856d0fae063
Kritish Pokharel <67674035+KritishPokharel@users.noreply.github.com>
2025-11-08 01:06:11 -0500

    Refactor App component to redirect instead of show todos
    Removed todo functionality and added redirect to ngrok URL.

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,12 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
+import { useEffect } from "react";
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
+    // 👇 Replace with your actual ngrok URL
+    window.location.replace("https://abcd1234.ngrok.io");
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+  return <p>Redirecting to your live site...</p>;
 }
 
 export default App;
